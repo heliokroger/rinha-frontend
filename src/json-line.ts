af810cb8c2063bb7ad2df4b4b9006ca1e44ad132
Helio Kroger <me@heliokroger.com>
2023-10-28 06:38:05 +0100

    feat: adding new components in vanilla

---
+++ src/json-line.ts
@@ -0,0 +1,85 @@
+import type { JsonLine } from "./types";
+import styles from "./json-line.module.scss";
+
+const IDENTATION_SPACING = 15;
+const IDENTATION_LINE_OFFSET = 2;
+
+const getValueType = (value: string) => {
+  const firstChar = value[0];
+
+  if (value.startsWith('"')) return "string";
+  if (value === "null") return "null";
+  if (value === "false" || value === "true") return "boolean";
+  if (
+    firstChar === "{" ||
+    firstChar === "[" ||
+    firstChar === "}" ||
+    firstChar === "]"
+  )
+    return "bracket";
+
+  return "number";
+};
+
+export const createListItem = (line: JsonLine) => {
+  const { content, nestLevel, arrayIndex } = line;
+
+  const $li = document.createElement("li");
+  $li.className = styles.line;
+
+  const tokens =
+    /"(.*)":\s?(".*"|-?[0-9]+(.?[0-9]+)?|null|true|false|{|\[)(,)?/gm.exec(
+      content
+    );
+
+  const lines = Array.from({ length: nestLevel }).map((_, i) => {
+    const $line = document.createElement("div");
+    $line.className = styles["identation-line"];
+    $line.style.left = `${i * IDENTATION_SPACING + IDENTATION_LINE_OFFSET}px`;
+
+    return $line;
+  });
+
+  for (const $line of lines) {
+    $li.appendChild($line);
+  }
+
+  const $span = document.createElement("span");
+  $span.className = styles["line-content"];
+  $span.style.marginLeft = `${nestLevel * IDENTATION_SPACING}px`;
+
+  if (arrayIndex !== undefined) {
+    const $arrayIndex = document.createElement("span");
+    $arrayIndex.className = styles["array-index"];
+    $arrayIndex.textContent = `${arrayIndex}: `;
+
+    $span.appendChild($arrayIndex);
+  }
+
+  const $value = document.createElement("span");
+
+  if (tokens !== null) {
+    const [, key, value] = tokens;
+    const $objectKey = document.createElement("span");
+    $objectKey.className = styles["object-key"];
+    $objectKey.textContent = key;
+
+    const $colon = document.createElement("span");
+    $colon.textContent = ": ";
+
+    $span.appendChild($objectKey);
+    $span.appendChild($colon);
+
+    $value.className = `${styles.value} ${styles[getValueType(value)]}`;
+    $value.textContent = value;
+  } else {
+    $value.className = `${styles.value} ${styles[getValueType(content)]}`;
+    $value.textContent = content.endsWith(",") ? content.slice(0, -1) : content;
+  }
+
+  $span.appendChild($value);
+
+  $li.appendChild($span);
+
+  return $li;
+};
---
+++ src/tree-viewer.ts
@@ -0,0 +1,72 @@
+import { createListItem } from "./json-line";
+import { parseJson, state as parserState } from "./parse-json";
+import styles from "./tree-viewer.module.scss";
+import type { JsonLine } from "./types";
+
+const getListItems = (lines: JsonLine[]) => {
+  const fragment = document.createDocumentFragment();
+
+  for (const line of lines) {
+    const $listItem = createListItem(line);
+    fragment.appendChild($listItem);
+  }
+
+  return fragment;
+};
+
+type State = {
+  observer: IntersectionObserver | null;
+  from: number;
+  to: number;
+};
+
+const state: State = { observer: null, from: 0, to: 100 };
+
+export const getTreeViewer = (file: File) => {
+  const $section = document.createElement("section");
+  $section.className = styles.content;
+
+  const $fileName = document.createElement("h2");
+  $fileName.className = styles["file-name"];
+  $fileName.textContent = file.name;
+
+  $section.appendChild($fileName);
+
+  const $ul = document.createElement("ul");
+  $ul.className = styles["list-container"];
+
+  $section.appendChild($ul);
+
+  $ul.appendChild(getListItems(parserState.lines));
+
+  const $trigger = document.createElement("div");
+  $trigger.style.height = "10px";
+  $section.appendChild($trigger);
+
+  state.observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      state.from += 50;
+      state.to += 50;
+
+      const hasLoadedIndex = parserState.lines[state.to];
+
+      if (hasLoadedIndex) {
+        const slice = parserState.lines.slice(state.from, state.to);
+        $ul.appendChild(getListItems(slice));
+      } else {
+        parseJson({
+          reset: false,
+          file,
+          numberOfRows: state.to,
+        }).then(() => {
+          const slice = parserState.lines.slice(state.from, state.to);
+          $ul.appendChild(getListItems(slice));
+        });
+      }
+    }
+  });
+
+  state.observer.observe($trigger);
+
+  return $section;
+};
